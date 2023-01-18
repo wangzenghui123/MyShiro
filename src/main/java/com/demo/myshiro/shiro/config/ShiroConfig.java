19bfb9b75fcb9ba0108daa2866a0c7e767f241f0
wangzenghui <1573713057@qq.com>
2023-01-18 22:24:24 +0800

    home page tab

--- src/main/java/com/demo/myshiro/shiro/config/ShiroConfig.java
+++ src/main/java/com/demo/myshiro/shiro/config/ShiroConfig.java
@@ -5,24 +5,15 @@ import com.demo.myshiro.shiro.cache.CustomCacheManager;
 import com.demo.myshiro.shiro.credential.CustomHashCredentialMatcher;
 import com.demo.myshiro.shiro.filter.CustomShiroFilter;
 import com.demo.myshiro.shiro.realm.CustomRealm;
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-import org.apache.shiro.authz.Authorizer;
-import org.apache.shiro.authz.ModularRealmAuthorizer;
 import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-
 import javax.servlet.Filter;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
